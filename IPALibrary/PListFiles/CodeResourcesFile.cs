0d188ed03d84a1ba2bec7eb6f8028ed57f79561f
ROM-Knowledgeware <tal@kmrom.com>
2018-10-25 18:19:25 +0200

    CodeResources: Use SHA256 in addition to SHA1

--- IPALibrary/PListFiles/CodeResourcesFile.cs
+++ IPALibrary/PListFiles/CodeResourcesFile.cs
@@ -1,4 +1,4 @@
-/* Copyright (C) 2017 ROM Knowledgeware. All rights reserved.
+/* Copyright (C) 2017-2018 ROM Knowledgeware. All rights reserved.
  * 
  * You can redistribute this program and/or modify it under the terms of
  * the GNU Lesser Public License as published by the Free Software Foundation,
@@ -30,16 +30,20 @@ namespace IPALibrary
         public void UpdateFileHash(string fileName, byte[] fileBytes)
         {
             DictionaryNode filesNode = PListHelper.GetDictionaryValueFromPList(RootNode, "files");
-            byte[] hash = new SHA1Managed().ComputeHash(fileBytes);
+            byte[] sha1Hash = new SHA1Managed().ComputeHash(fileBytes);
+            byte[] sha256Hash = new SHA256Managed().ComputeHash(fileBytes);
             if (filesNode.ContainsKey(fileName))
             {
-                filesNode[fileName] = new DataNode(hash);
+                filesNode[fileName] = new DataNode(sha1Hash);
             }
 
             DictionaryNode files2Node = PListHelper.GetDictionaryValueFromPList(RootNode, "files2");
             if (files2Node != null && files2Node.ContainsKey(fileName))
             {
-                files2Node[fileName] = new DataNode(hash);
+                DictionaryNode entryNode = new DictionaryNode();
+                entryNode.Add("hash", new DataNode(sha1Hash));
+                entryNode.Add("hash2", new DataNode(sha256Hash));
+                files2Node[fileName] = entryNode;
             }
         }
     }
