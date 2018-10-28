9621138ed049fe305e135517936ba03b2d5657ab
ROM-Knowledgeware <tal@kmrom.com>
2018-10-28 18:42:32 +0200

    CodeSignatureEntryType enum: added AlternateCodeDirectory1

--- IPALibrary/CodeSignature/Enums/CodeSignatureEntryType.cs
+++ IPALibrary/CodeSignature/Enums/CodeSignatureEntryType.cs
@@ -7,6 +7,7 @@ namespace IPALibrary.CodeSignature
         CodeDirectory = 0x00000000, // CSSLOT_CODEDIRECTORY, The signature of this entry type will be CSMAGIC_CODEDIRECTORY
         Requirements = 0x00000002,  // CSSLOT_REQUIREMENTS,  The signature of this entry type will be CSMAGIC_REQUIREMENTS
         Entitlements = 0x00000005,  // CSSLOT_ENTITLEMENTS,  The signature of this entry type will be CSMAGIC_EMBEDDED_ENTITLEMENTS
+        AlternateCodeDirectory1 = 0x00001000, // CSSLOT_ALTERNATE_CODEDIRECTORIES, The signature of this entry type will be CSMAGIC_CODEDIRECTORY
         CmsSignature = 0x00010000,  // CSSLOT_SIGNATURESLOT, The signature of this entry type will be CSMAGIC_BLOBWRAPPER
     }
 }
