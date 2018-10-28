c918fcaad66ad2fa07fb140c631f1dfe94a58443
ROM-Knowledgeware <tal@kmrom.com>
2018-10-28 20:35:19 +0200

    CodeSignatureHelper: Added compile-time option to create Alternative CodeDirectoryBlob with SHA256 hashes

--- IPALibrary/CodeSignature/Helpers/CodeSignatureHelper.cs
+++ IPALibrary/CodeSignature/Helpers/CodeSignatureHelper.cs
@@ -11,9 +11,13 @@ using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using System.Security.Cryptography;
+using Org.BouncyCastle.Asn1;
+using Org.BouncyCastle.Asn1.Cms;
 using Org.BouncyCastle.Pkcs;
 using Org.BouncyCastle.X509;
 using IPALibrary.MachO;
+using PListNet;
+using PListNet.Nodes;
 using Utilities;
 
 namespace IPALibrary.CodeSignature
@@ -134,16 +138,24 @@ namespace IPALibrary.CodeSignature
             CodeSignatureCommand command = (CodeSignatureCommand)file.LoadCommands[file.LoadCommands.Count - 1];
             int codeLength = (int)command.DataOffset;
             CodeDirectoryBlob codeDirectory = CreateCodeDirectoryBlob(codeLength, bundleIdentifier, teamID, HashType.SHA1);
+            AttributeTable signedAttributesTable = null;
+#if ALT_CODE_DIRECTORY_SHA256
+            CodeDirectoryBlob alternativeCodeDirectory1 = CreateCodeDirectoryBlob(codeLength, bundleIdentifier, teamID, HashType.SHA256);
+            signedAttributesTable = GenerateSignedAttributesTable(codeDirectory.GetBytes(), alternativeCodeDirectory1.GetBytes());
+#endif
             CodeRequirementsBlob codeRequirements = CreateCodeRequirementsBlob(bundleIdentifier, certificateCN);
             EntitlementsBlob entitlementsBlob = CreateEntitlementsBlob(entitlements);
             CmsSignatureBlob cmsSignature = new CmsSignatureBlob();
             // We create a dummy signature to determine the length required
-            cmsSignature.Data = CMSHelper.GenerateSignature(certificateChain, privateKey, codeDirectory.GetBytes());
+            cmsSignature.Data = CMSHelper.GenerateSignature(certificateChain, privateKey, codeDirectory.GetBytes(), signedAttributesTable);
 
             CodeSignatureSuperBlob codeSignature = new CodeSignatureSuperBlob();
             codeSignature.Entries.Add(CodeSignatureEntryType.CodeDirectory, codeDirectory);
             codeSignature.Entries.Add(CodeSignatureEntryType.Requirements, codeRequirements);
             codeSignature.Entries.Add(CodeSignatureEntryType.Entitlements, entitlementsBlob);
+#if ALT_CODE_DIRECTORY_SHA256
+            codeSignature.Entries.Add(CodeSignatureEntryType.AlternateCodeDirectory1, alternativeCodeDirectory1);
+#endif
             codeSignature.Entries.Add(CodeSignatureEntryType.CmsSignature, cmsSignature);
 
             command.DataSize = (uint)codeSignature.Length;
@@ -152,7 +164,11 @@ namespace IPALibrary.CodeSignature
 
             byte[] codeToHash = ByteReader.ReadBytes(file.GetBytes(), 0, codeLength);
             UpdateSpecialHashes(codeDirectory, codeToHash, infoFileBytes, codeRequirements, codeResourcesBytes, entitlementsBlob);
-            cmsSignature.Data = CMSHelper.GenerateSignature(certificateChain, privateKey, codeDirectory.GetBytes());
+#if ALT_CODE_DIRECTORY_SHA256
+            UpdateSpecialHashes(alternativeCodeDirectory1, codeToHash, infoFileBytes, codeRequirements, codeResourcesBytes, entitlementsBlob);
+            signedAttributesTable = GenerateSignedAttributesTable(codeDirectory.GetBytes(), alternativeCodeDirectory1.GetBytes());
+#endif
+            cmsSignature.Data = CMSHelper.GenerateSignature(certificateChain, privateKey, codeDirectory.GetBytes(), signedAttributesTable);
 
             // Store updated code signature:
             byte[] codeSignatureBytes = codeSignature.GetBytes();
@@ -240,5 +256,40 @@ namespace IPALibrary.CodeSignature
             entitlementsBlob.Data = entitlements.GetBytes(PListNet.PListFormat.Xml);
             return entitlementsBlob;
         }
+
+#if ALT_CODE_DIRECTORY_SHA256
+        private static byte[] GenerateHashesPList(byte[] codeDirectoryBytes, byte[] alternativeCodeDirectory1Bytes)
+        {
+            PListFile plist = new PListFile();
+            DictionaryNode rootNode = new DictionaryNode();
+            ArrayNode hashesNode = new ArrayNode();
+            hashesNode.Add(new DataNode(HashAlgorithmHelper.ComputeHash(HashType.SHA1, codeDirectoryBytes)));
+            hashesNode.Add(new DataNode(HashAlgorithmHelper.ComputeHash(HashType.SHA256Truncated, alternativeCodeDirectory1Bytes)));
+            rootNode.Add("cdhashes", hashesNode);
+            plist.RootNode = rootNode;
+            return plist.GetBytes(PListFormat.Xml);
+        }
+
+        private static AttributeTable GenerateSignedAttributesTable(byte[] codeDirectoryBytes, byte[] alternativeCodeDirectory1Bytes)
+        {
+            DerObjectIdentifier hashesAttributeIdentifier = new DerObjectIdentifier("1.2.840.113635.100.9.2");
+            DerObjectIdentifier sha256Identifier = new DerObjectIdentifier("2.16.840.1.101.3.4.2.1");
+            DerObjectIdentifier sha1Identifier = new DerObjectIdentifier("1.3.14.3.2.26");
+            DerObjectIdentifier hashesPlistIdentifier = new DerObjectIdentifier("1.2.840.113635.100.9.1");
+            byte[] codeDirectory1Sha256Hash = new SHA256Managed().ComputeHash(alternativeCodeDirectory1Bytes);
+            byte[] codeDirectorySha1Hash = new SHA1Managed().ComputeHash(codeDirectoryBytes);
+
+            BerSequence sha256Sequence = new BerSequence(sha256Identifier, new DerOctetString(codeDirectory1Sha256Hash));
+            BerSequence sha1Sequence = new BerSequence(sha1Identifier, new DerOctetString(codeDirectorySha1Hash));
+            BerSet codeDirectoryHashSet = new BerSet(new Asn1EncodableVector(sha256Sequence, sha1Sequence));
+            byte[] hashesPListBytes = GenerateHashesPList(codeDirectoryBytes, alternativeCodeDirectory1Bytes);
+            DerSet hashesPlistSet = new DerSet(new DerOctetString(hashesPListBytes));
+
+            Asn1EncodableVector signedAttributes = new Asn1EncodableVector();
+            signedAttributes.Add(new Org.BouncyCastle.Asn1.Cms.Attribute(hashesAttributeIdentifier, codeDirectoryHashSet));
+            signedAttributes.Add(new Org.BouncyCastle.Asn1.Cms.Attribute(hashesPlistIdentifier, hashesPlistSet));
+            return new AttributeTable(signedAttributes);
+        }
+#endif
     }
 }
